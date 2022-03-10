6982a3cd13b4e6cb22cac03f0b0e958f64e1d081
Olwenn-Alliaume <olwenn.alliaumegarcia@everis.nttdata.com>
2022-03-10 10:13:24 +0100

    Agregar archivos de proyecto.

---
+++ WizLib_DataAccess/Data/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizLib_Model.Models;
+
+namespace WizLib_DataAccess.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ WizLib_Model/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WizLib_Model.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
